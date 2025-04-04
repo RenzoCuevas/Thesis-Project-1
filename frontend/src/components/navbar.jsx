c5fbd3b0429b9550cd3dd67d9e1ab79fae035f57
RenzoCuevas <cuevas_renzo@plpasig.edu.ph>
2025-04-04 23:34:50 +0800

    Dashboard modified

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom";
+import './App.css';
 
 export default function Navbar() {
   return (
     <nav className="bg-blue-600 text-white p-4 flex justify-between items-center">
       <h1 className="text-2xl font-bold">LMS</h1>
       <div className="space-x-4">
-        <Link to="/" className="hover:underline">Home</Link>
-        <Link to="/dashboard" className="hover:underline">Dashboard</Link>
-        <Link to="/upload" className="hover:underline">Upload</Link> 
-        <Link to="/register" className="hover:underline">Register</Link>
-        <Link to="/login" className="hover:underline">Login</Link>
+        <Link to="/" className="hover:underline navContents">Home</Link>
+        <Link to="/dashboard" className="hover:underline navContents">Dashboard</Link>
+        <Link to="/upload" className="hover:underline navContents">Upload</Link> 
+        <Link to="/register" className="hover:underline navContents">Register</Link>
+        <Link to="/login" className="hover:underline navContents">Login</Link>
       </div>
     </nav>
   );
